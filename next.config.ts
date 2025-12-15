60894b6c5c1abf9bf756d08643cdee512551bef9
ian_alvarenga11 <ian.alvarenga@novahaus.com.br>
2025-12-15 16:56:33 -0300

    fix: remove eslint from next.config for Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
-    // WARNING: Isso permite que o build seja concluído mesmo com erros de ESLint
-    ignoreDuringBuilds: true,
-  },
   /* config options here */
 };
 
